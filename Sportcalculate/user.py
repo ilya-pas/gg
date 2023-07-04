d755b608b035e44007373d08cc174195871f09b2
ilya-pas <107921734+ilya-pas@users.noreply.github.com>
2023-07-04 10:11:33 +0500

    Delete Sportcalculate directory

--- Sportcalculate/user.py
+++
@@ -1,8 +0,0 @@
-class User:
-    def __init__(self, name, strength, endurance, speed, flexibility, coordination):
-        self.name = name
-        self.strength = strength
-        self.endurance = endurance
-        self.speed = speed
-        self.flexibility = flexibility
-        self.coordination = coordination
